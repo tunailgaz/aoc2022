b3e3e7744dc187bb9679a2102b91295c6d808486
tunailgaz <tunailgaz@gmail.com>
2023-02-28 09:35:43 +0300

    wip

--- zig/src/playground.zig
+++ zig/src/playground.zig
@@ -3,16 +3,15 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
+    const stdin = std.io.getStdIn().reader();
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
-
-    const allocator = arena.allocator();
+    var allocator = arena.allocator();
 
     // const byte: usize = std.math.maxInt(@TypeOf(myval));
 
-    const data = @embedFile("data/day3");
-
-    var it = std.mem.split(u8, data, "\n");
+    var input = try stdin.readAllAlloc(allocator, 2048 * 2048);
+    var it = std.mem.tokenize(u8, input, "\n");
 
     var lineList = std.ArrayList([]const u8).init(allocator);
     defer lineList.deinit();
@@ -22,20 +21,26 @@ pub fn main() !void {
 
     var gLimit: u8 = 2;
     var gCounter: u8 = 0;
-    while (it.next()) |line| {
+    var tt: u32 = 0;
+
+    var gg: [][]const u8 = undefined;
+    while (it.next()) |line| : (tt += 1) {
         try lineList.append(line[0..]);
         gCounter += 1;
         if (gCounter > gLimit) {
+            // try groupList.append(lineList.items[0..]);
             try groupList.append(lineList.items[lineList.items.len - 3 .. lineList.items.len]);
+            gg[tt] = line;
+            groupList.clearAndFree();
             gCounter = 0;
         }
     }
 
+    // intersting
+    // https://github.com/jordanlewis/aoc/blob/main/2022/src/day24.zig
+
     print("total: {} \n", .{groupList.items.len});
     print("total: {} \n", .{groupList.items.len});
-    for (groupList.items) |gItems| {
-        _ = gItems;
-        // print("gitems: {s} \n", .{gItems});
-    }
+
     // print("gitems: {s} \n", .{gItems});
 }
