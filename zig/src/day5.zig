4a6828de3f4ea2ff027e14813a0e2251e53925e5
Tuna Ilgaz <tunailgaz@gmail.com>
2023-03-08 02:05:04 +0300

    day5 jordan lewis

--- zig/src/day5.zig
+++ zig/src/day5.zig
@@ -1,84 +1,55 @@
 const std = @import("std");
 const print = std.debug.print;
+const ArrayList = std.ArrayList;
 const mem = std.mem;
 
-const Stack = struct { crates: std.ArrayList };
+pub fn main() !void {
+    const data = @embedFile("data/day5");
 
-fn parseCrates(crates: []const u8) !u32 {
-    var it = mem.tokenize(u8, crates, "\n");
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
-
     const allocator = arena.allocator();
 
-    var list: [10]std.ArrayList(u8) = .{std.ArrayList(u8).init(allocator)} ** 10;
-    // var bus1: [9]std.ArrayList(u8) = .{std.ArrayList(u8).init(allocator)} ** 9;
-    // var stacks: []std.ArrayList = undefined;
+    var lines_it = mem.split(u8, data, "\n");
 
-    var j: u8 = 0;
-    while (it.next()) |crate| {
-        list[j] = std.ArrayList(u8).init(allocator);
-        if (std.mem.containsAtLeast(u8, crate, 1, "[")) {
-            print("\ncrate: {s}\n", .{crate});
+    var buckets1: [9]ArrayList(u8) = .{ArrayList(u8).init(allocator)} ** 9;
+    var buckets2: [9]ArrayList(u8) = .{ArrayList(u8).init(allocator)} ** 9;
 
-            var pos: u8 = 1;
-            var i: u8 = 0;
-            for (crate[0..], 0..) |_, idx| {
-                i += 1;
-                if (!mem.eql(u8, crate[idx .. idx + 1], " ") and !mem.eql(u8, crate[idx .. idx + 1], "[") and !mem.eql(u8, crate[idx .. idx + 1], "]")) {
-                    // _ = idx;
-
-                    try list[j].insert(0, crate[idx]);
-                    print(" [{}]{} -> {c} ", .{ pos, idx, crate[idx] });
-                }
-                if (i > 3) {
-                    pos += 1;
-                    i = 0;
-                }
+    while (lines_it.next()) |line| {
+        if (line.len == 0 or line[0] == ' ') continue;
+        if (line[0] == '[') {
+            var iter = std.mem.tokenize(u8, line, "[] ");
+            while (iter.next()) |x| {
+                print("x-> [{}] {s}\n", .{ iter.index / 4, x });
+                try buckets1[iter.index / 4].insert(0, x[0]);
+                try buckets2[iter.index / 4].insert(0, x[0]);
             }
-
-            // print(" {s} {s} {s} \n", .{ crate[1..2], crate[4..5], crate[7..8] });
-            // var cargo_it = mem.tokenize(u8, crate, "[]");
-            // var cargo_idx: u8 = 0;
-            // while (cargo_it.next()) |cargo| {
-            //     cargo_idx += 1;
-            //     if (cargo.len == 1 and !mem.eql(u8, cargo, " ")) {
-            //         print("\ncargo: '{s}' {} | all '{s}' ", .{ cargo, (cargo_idx), cargo });
-            //     }
-            // }
-
-            print("\n", .{});
-        } else {
-            print("end of story\n", .{});
+            print("{s}\n", .{buckets1[0].items});
+            continue;
         }
-        print("list[{}]: {s}\n", .{ j, list[j].items });
-        j += 1;
-        // var cargo_it = mem.tokenize(u8, crate, "[ ]");
 
-        // // var cargo_idx: u8 = 0;
-        // while (cargo_it.next()) |cargo| {
-        //     print(" {s} ", .{cargo});
-        // }
+        var iter = std.mem.tokenize(u8, line, " movefromto");
+        var n = try std.fmt.parseInt(usize, iter.next().?, 10);
+        const src = try std.fmt.parseInt(usize, iter.next().?, 10) - 1;
+        const dst = try std.fmt.parseInt(usize, iter.next().?, 10) - 1;
+
+        const origN = n;
+        while (n > 0) : (n -= 1) {
+            const x = buckets1[src].pop();
+            try buckets1[dst].append(x);
+        }
+        n = origN;
+        while (n > 0) : (n -= 1) {
+            try buckets2[dst].append(buckets2[src].items[buckets2[src].items.len - n]);
+        }
+        buckets2[src].items.len -= origN;
     }
-
-    return 3;
-}
-
-pub fn main() !void {
-    const data = @embedFile("data/day5.example");
-
-    var lines_it = mem.split(u8, data, "\n\n");
-
-    var crates_it = mem.tokenize(u8, lines_it.next().?, "\n");
-    // var moves = mem.tokenize(u8, lines_it.next().?, "\n");
-
-    // _ = moves;
-    // print("crates len:{}\n", .{crates.});
-    const crates = try parseCrates(crates_it.rest());
-    _ = crates;
-
-    // print("\n-----------\n", .{});
-    // while (moves.next()) |move| {
-    //     print("move: {s}\n", .{move});
-    // }
+    for ([_][9]ArrayList(u8){ buckets1, buckets2 }, 0..) |bs, i| {
+        print("part {d}: ", .{i});
+        for (bs) |b| {
+            const s = [_]u8{b.items[b.items.len - 1]};
+            print("{s}", .{s});
+        }
+        print("\n", .{});
+    }
 }
