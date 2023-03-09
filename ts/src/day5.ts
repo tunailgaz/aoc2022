7dd4e298d4edc7bc3587c3269a584cf4203a9315
Tuna Ilgaz <tunailgaz@gmail.com>
2023-03-09 22:49:48 +0300

    day 5 typescript part2

--- ts/src/day5.ts
+++ ts/src/day5.ts
@@ -17,11 +17,12 @@ const getSize = (stacks: Array<string>): number => {
   const move_lines = file_parts[1]?.split("\n");
   const stack_size = getSize(stack_lines);
 
+  let part_1_list: string[][] = [];
+  let part_2_list: string[][] = [];
 
-
-  let array_list: string[][] = [];
   for(let i=0; i<stack_size; i++) {
-    array_list[i] = [];
+    part_1_list[i] = [];
+    part_2_list[i] = [];
   }
 
   for(let line of stack_lines.reverse()) {
@@ -29,32 +30,42 @@ const getSize = (stacks: Array<string>): number => {
       for(let i=0; i<stack_size; i++) {
         let cargo = line.slice(4 * i, 4 * i + 4 );
         if(cargo.trim()){
-          array_list[i].push(cargo.trim().replace(/[^A-Z]/g,''))
+          part_1_list[i].push(cargo.trim().replace(/[^A-Z]/g,''))
+          part_2_list[i].push(cargo.trim().replace(/[^A-Z]/g,''))
         }
       }
     }
   }
+
   for(let move_line of move_lines) {
     const move_line_numbers  = move_line.split(/\s/g);
     if(move_line_numbers?.length) {
-      let move = Number(move_line_numbers[1]);
+      const move = Number(move_line_numbers[1]);
       const from = Number(move_line_numbers[3]) -1 ;
       const to = Number(move_line_numbers[5]) -1 ;
-      while(move > 0) {
-        let item = array_list[from].pop();
+
+      for(let i=0; i<move; i++){
+        let item = part_1_list[from].pop();
         if(item){
-          array_list[to].push(item);
+          part_1_list[to].push(item);
         }
-        move--;
       }
+      part_2_list[to] =  [...part_2_list[to], ...part_2_list[from].slice(move * -1)];
+      part_2_list[from] = part_2_list[from].slice(0,part_2_list[from].length - move);
     }
 
   }
+
   let part1 = '';
-  for(let list of array_list) {
+  for(let list of part_1_list) {
     part1 += list[list.length -1];
   }
-
   console.log(part1)
+
+  let part2 = '';
+  for(let list of part_2_list) {
+    part2 += list[list.length -1];
+  }
+  console.log(part2)
 
 })();
