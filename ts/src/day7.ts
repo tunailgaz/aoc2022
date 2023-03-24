a8542c372628ed83cf547aba5ec2dc0f4fc62cf3
Tuna Ilgaz <tunailgaz@gmail.com>
2023-03-24 08:12:28 +0300

    wip

--- ts/src/day7.ts
+++ ts/src/day7.ts
@@ -1,125 +1,124 @@
 import path from "path";
 import fs from "node:fs";
 
-
 interface File {
-  name: string
-  size: number
+  name: string;
+  size: number;
 }
 
 interface Folder {
-  name: string
-  files: File[]
-  folders: Folder[]
-  parent: Folder | null
+  name: string;
+  files: File[];
+  folders: Folder[];
+  parent: Folder | null;
 }
 
-let part_1_candidates: { name: string, size: number }[] = [];
-let part_2_candidates: { name: string, size: number }[] = [];
+let part1Candidates: { name: string; size: number }[] = [];
+let part2Candidates: { name: string; size: number }[] = [];
 
-const total_size = 70000000;
-const required_size = 30000000;
+const totalSize = 70000000;
+const requiredSize = 30000000;
 
+const calculateFolderSize = (folder: Folder, needSize: number): number => {
 
-const calc_folder_size = (folder: Folder, need_size: number): number => {
-  let current_size = 0;
-  for (let file of folder.files) {
-    current_size += file.size;
-  }
-  if (folder.folders.length) {
-    for (let f of folder.folders) {
-      current_size += calc_folder_size(f, need_size)
-    }
-  }
-  if (current_size <= 100000) {
-    part_1_candidates = [...part_1_candidates, {
+  let currentSize = folder.files.reduce((accumulator, file) => accumulator + file.size, 0)
+ currentSize += folder.folders.reduce((acc, f) => acc + calculateFolderSize(f, needSize), 0);
+  if (currentSize <= 100000) {
+    part1Candidates.push({
       name: folder.name,
-      size: current_size,
-    }]
-  }
-  if (need_size && current_size >= need_size) {
-    part_2_candidates = [...part_2_candidates, {name: folder.name, size: current_size}];
+      size: currentSize,
+    });
   }
 
-  return current_size
-}
+  if (needSize && currentSize >= needSize) {
+    part2Candidates.push({ name: folder.name, size: currentSize });
+  }
 
+  return currentSize;
+};
 
-(() => {
-
-  // const file_path = path.join(__dirname, 'data/day7.example');
-  const file_path = path.join(__dirname, 'data/day7');
-  const input = fs.readFileSync(file_path, 'utf-8')
-  const lines = input.split('\n');
+(async () => {
+  const fileDir = path.join(__dirname, "data/day7");
+  const input = await fs.promises.readFile(fileDir, "utf-8");
+  const lines = input.split("\n");
 
   let rootFolder: Folder = {
-    name: '/',
+    name: "/",
     files: [],
     folders: [],
     parent: null,
-  }
+  };
 
   let currentFolder = rootFolder;
-  for (let i = 1; i < lines.length; i++) {
-    let line = lines[i].trim();
 
-    if (line && line[0] === '$') {
-      let command = line.split(' ')[1]?.trim();
+  for (let i = 1; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    if (line && line[0] === "$") {
+      const command = line.split(" ")[1]?.trim();
 
       switch (command) {
-        case 'cd': {
-          let args = line.split(' ')[2]?.trim()
-          if (args === '..') {
+        case "cd": {
+          const args = line.split(" ")[2]?.trim();
+
+          if (args === "..") {
             if (currentFolder.parent) {
               currentFolder = currentFolder.parent;
             }
           } else {
-            let folder = currentFolder.folders?.find(t => t.name === args)
+            const folder = currentFolder.folders?.find(
+              (t) => t.name === args
+            );
+
             if (folder) {
               currentFolder = folder;
             } else {
-              let new_folder: Folder = {
+              const newFolder: Folder = {
                 name: args,
                 folders: [],
                 files: [],
                 parent: currentFolder,
-              }
-              currentFolder.folders.push(new_folder);
-              currentFolder = new_folder;
+              };
+
+              currentFolder.folders.push(newFolder);
+              currentFolder = newFolder;
             }
           }
 
           break;
-
         }
-        case 'ls': {
+        case "ls": {
           let j = 1;
+
           while (true) {
-            let line = lines[i + j];
-            if (!line || line[0] === '$') {
+            const line = lines[i + j];
+
+            if (!line || line[0] === "$") {
               break;
             }
-            j++
 
-            let ls_command = line.split(' ');
-            if (ls_command[0].trim() === 'dir') {
-              let new_folder: Folder = {
-                name: ls_command[1].trim(),
+            j++;
+
+            const lsCommand = line.split(" ");
+
+            if (lsCommand[0].trim() === "dir") {
+              const newFolder: Folder = {
+                name: lsCommand[1].trim(),
                 folders: [],
                 files: [],
                 parent: currentFolder,
-              }
-              currentFolder.folders.push(new_folder)
-
+              };
+              currentFolder.folders.push(newFolder);
             } else {
-              let size = Number(ls_command[0].trim());
-              let name = ls_command[1].trim();
-              let file: File = {
-                name,
-                size
-              }
-              currentFolder.files.push(file)
+              const size = Number(lsCommand[0].trim());
+              const name = lsCommand[1].trim();
 
+              const file: File = {
+                name,
+                size,
+              };
+
+              currentFolder.files.push(file);
             }
           }
           break;
@@ -129,11 +128,12 @@ const calc_folder_size = (folder: Folder, need_size: number): number => {
   }
 
   // add part1 candidates
-  let rootSize = calc_folder_size(rootFolder, 0)
-  console.log(`part1: ${part_1_candidates.map(t => t.size).reduce((a, b) => b + a, 0)}`)
+  const rootSize = calculateFolderSize(rootFolder, 0);
+  console.log(
+    `part1: ${part1Candidates.map((t) => t.size).reduce((a, b) => b + a, 0)}`
+  );
 
   // add part2 candidates
-  calc_folder_size(rootFolder, required_size - (total_size - rootSize))
-  console.log(`part2: ${Math.min(...part_2_candidates.map(t => t.size))}`)
-
+  calculateFolderSize(rootFolder, requiredSize - (totalSize - rootSize));
+  console.log(`part2: ${Math.min(...part2Candidates.map((t) => t.size))}`);
 })();
--- zig/src/day7.zig
+++ zig/src/day7.zig
@@ -1,33 +1,94 @@
-const std = @import("std");
-const print = std.debug.print;
-
-const data = @embedFile("data/day7.example");
-
-pub fn main() !void {
-    // const stdin = std.io.getStdIn().reader();
-    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    // defer arena.deinit();
-
-    // const allocator = arena.allocator();
-    // _ = allocator;
-
-    // var buffer: [1024]u8 = undefined;
-    // while (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |line| {
-    //     if (line[0] == '$') {
-    //         // print("yep \n", .{});
-    //     }
-    //     // print(" line: '{s}' \n", .{line});
-    // }
-
-    var lines_it = std.mem.split(u8, data, "\n");
-
-    // var commands = std.ArrayList([][]const u8);
-
-    print("\n", .{});
-    while (lines_it.next()) |line| {
-        if (line[0] == '$') {
-            // print("yesss", .{});
-        }
-        print(" line: {s} \n", .{line});
-    }
-}
+// const std = @import("std");
+// const Path = std.fs.Path;
+// const fs = std.fs;
+//
+// pub const File = struct {
+//     name: []const u8,
+//     size: u64,
+// };
+//
+// pub const Folder = struct {
+//     name: []const u8,
+//     files: []File,
+//     folders: []Folder,
+//     parent: *Folder,
+// };
+//
+// var part_1_candidates: []File = undefined;
+// var part_2_candidates: []File = undefined;
+//
+// const total_size: u64 = 70000000;
+// const required_size: u64 = 30000000;
+//
+// fn calc_folder_size(folder: *Folder, need_size: u64) u64 {
+//     var current_size: u64 = 0;
+//     for (folder.files) |file| {
+//         current_size += file.size;
+//     }
+//     for (folder.folders) |f| {
+//         current_size += calc_folder_size(f, need_size);
+//     }
+//     if (current_size <= 100000) {
+//         part_1_candidates |= File{ .name = folder.name, .size = current_size };
+//     }
+//     if (need_size > 0 and current_size >= need_size) {
+//         part_2_candidates |= File{ .name = folder.name, .size = current_size };
+//     }
+//     return current_size;
+// }
+//
+// pub fn main() anyerror!void {
+//     const rootFolder = Folder{ .name = "/", .files = null, .folders = null, .parent = null };
+//     var currentFolder: *Folder = &rootFolder;
+//       const data = @embedFile("data/day4");
+//         // var lines_it = mem.tokenize(u8, "2-4,6-8\n2-3,4-5", "\n");
+//     var lines = mem.tokenize(u8, data, "\n");
+//     // const lines = try input.split(u8, "\n");
+//
+//     while(lines.next()) |line| {
+//         const trimmed = try line.trim();
+//             if (trimmed == "") {
+//                 continue;
+//             }
+//             const parts = try std.mem.split(trimmed, " ", .{});
+//             if (parts[0] == "$cd") {
+//                 const args = parts[1] catch unreachable;
+//                 if (args == "..") {
+//                     currentFolder = currentFolder.parent catch unreachable;
+//                 } else {
+//                     var folder = currentFolder.folders.find((folder) => folder.name == args);
+//                     if (folder == null) {
+//                         const new_folder = Folder{ .name = args, .files = null, .folders = null, .parent = currentFolder };
+//                         currentFolder.folders |= new_folder;
+//                         currentFolder = &currentFolder.folders[comptime @intCast(u32)(currentFolder.folders.len - 1)];
+//                     } else {
+//                         currentFolder = folder;
+//                     }
+//                 }
+//             } else if (parts[0] == "$ls") {
+//                 for (var i: usize = 1; i < parts.len; i += 2) {
+//                     if (parts[i] == "dir") {
+//                         const new_folder = Folder{ .name = parts[i + 1], .files = null, .folders = null, .parent = currentFolder };
+//                         currentFolder.folders |= new_folder;
+//                         currentFolder = &currentFolder.folders[comptime @intCast(u32)(currentFolder.folders.len - 1)];
+//                     } else {
+//                         const size = try std.fmt.parseInt(parts[i].toSlice());
+//                         const name = parts[i + 1];
+//                         const file = File{ .name = name, .size = size };
+//                         currentFolder.files |= file;
+//                     }
+//                 }
+//             }
+//     }
+//     // for (lines[1..]) |line| : (line.owned()) {
+//     //
+//     // }
+//
+//     const rootSize = calc_folder_size(&rootFolder, 0);
+//     const part1_size = part_1_candidates.reduce((acc, file) => acc + file.size, 0);
+//     std.log.info("part1: {}\n", .{part1_size});
+//
+//     calc_folder_size(&rootFolder, required_size - (total_size - rootSize));
+//     const part2_size = part_2_candidates.minByFn((file) => file.size).?.?.size catch unreachable;
+//     std.log.info("part2: {}\n", .{part2_size});
+// }
